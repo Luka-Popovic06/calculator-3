6b66a26f81b451ff70080dff5825dde305f4bb21
Luka Popovic <lukapopovic0111@gmail.com>
2025-12-26 22:26:45 +0100

    Add reusable Button component

---
+++ src/Button.jsx
@@ -0,0 +1,18 @@
+const Button = ({ children, variation, btnType, clickAction }) => {
+  const baseClass = "btn";
+  let btnClass;
+  if (variation === "standard") btnClass = "btn-standard";
+  if (variation === "primary") btnClass = "btn-primary";
+  if (variation === "secondary") btnClass = "btn-secondary";
+
+  return (
+    <button
+      type={btnType}
+      className={`${baseClass} ${btnClass}`}
+      onClick={clickAction}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
