731f5c363b735ae6da69ff49c818c1b37f035f2d
Luka Popovic <lukapopovic0111@gmail.com>
2025-12-26 22:25:14 +0100

    Add calculator buttons configuration

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const calculatorButtons = [
+  {
+    variation: "primary",
+    type: "button",
+    text: "CLEAR",
+  },
+  {
+    variation: "secondary",
+    type: "button",
+    text: "DELETE",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "7",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "8",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "9",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "/",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "4",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "5",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "6",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "*",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "1",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "2",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "3",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "-",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: ".",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "0",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "=",
+  },
+  {
+    variation: "standard",
+    type: "button",
+    text: "+",
+  },
+];
